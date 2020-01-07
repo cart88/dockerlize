88b221ee81a38dfb626db7bcbf1007ca55e51f09
luopeizhong <lpz2@163.com>
2020-01-07 09:30:06 +0800

    治理日志

--- code/db.php
+++ code/db.php
@@ -1,4 +1,7 @@
 <?php
+    print($ddd);
+    print("log_error:" . ini_get('log_errors'));
+    print("error_log:" . ini_get('error_log'));
     $link = mysqli_connect("mysql","admin","admin","mm");
     if (!$link) {
         echo "无法连接mysql" . PHP_EOL;
@@ -8,3 +11,5 @@
     }
     echo "连接成功";
     echo "主机信息：".mysqli_get_host_info($link).PHP_EOL;
+?>
+<img src="1.png" alt="dfsfs" >sss
